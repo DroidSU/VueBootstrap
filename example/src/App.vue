b2e856b596fdee2fcffe0a593444bf4bb62d254a
Sujoy Datta <sujoydatta26@gmail.com>
2019-08-02 08:14:11 +0530

    Update App.vue
    Added example login screen project for bootstrap vue

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,5 +1,82 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <b-navbar toggleable="lg" type="dark" class="navbar-background">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Link</b-nav-item>
+          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form>
+
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template slot="button-content">
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+
+    <br />
+
+    <b-container>
+      <b-row align-h="center" class="mt-5">
+        <b-card class="p-3">
+          <h3 class="mb-4">Login</h3>
+          <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+            <b-form-group id="input-group-1" label="Email address:" label-for="input-1">
+              <b-form-input
+                id="input-1"
+                v-model="form.email"
+                type="email"
+                required
+                placeholder="Enter email"
+              ></b-form-input>
+            </b-form-group>
+
+            <b-form-group id="input-group-2" label="Password:" label-for="input-2">
+              <b-form-input
+                id="input-2"
+                v-model="form.password"
+                required
+                placeholder="Enter password"
+                type="password"
+              ></b-form-input>
+            </b-form-group>
+
+            <b-form-group id="input-group-4">
+              <b-form-checkbox-group v-model="form.checked" id="checkboxes-4">
+                <b-form-checkbox value="me">Remember me</b-form-checkbox>
+              </b-form-checkbox-group>
+            </b-form-group>
+
+            <b-button type="submit" variant="primary">Submit</b-button>&nbsp;
+            <b-button type="reset" variant="danger">Reset</b-button>
+          </b-form>
+        </b-card>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
@@ -7,11 +84,45 @@ export default {
   name: "app",
   data() {
     return {
-      msg: "Welcome to Your Vue.js App"
+      form: {
+        email: "",
+        name: "",
+        food: null,
+        checked: []
+      },
+      show: true
     };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      // Reset our form values
+      this.form.email = "";
+      this.form.password = "";
+      this.form.checked = [];
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    }
   }
 };
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Lato:400,700");
+body {
+  background: #eef1f4;
+  font-family: "Lato", sans-serif;
+}
+
+.navbar-background {
+  background: #353535;
+  min-height: 20px;
+}
 </style>
